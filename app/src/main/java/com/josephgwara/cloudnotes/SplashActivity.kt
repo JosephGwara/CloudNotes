6bdec318b4f58e1a946012f367c064b1096f8339
JosephGwara <jgwarix@gmail.com>
2022-10-15 21:13:40 +0200

    Created Create Account activity and designed the create account screen

--- app/src/main/java/com/josephgwara/cloudnotes/SplashActivity.kt
+++ app/src/main/java/com/josephgwara/cloudnotes/SplashActivity.kt
@@ -11,7 +11,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, CreateAccountActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000) // 3000 is the delayed time in milliseconds.
